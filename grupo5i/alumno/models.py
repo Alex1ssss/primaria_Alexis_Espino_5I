9cb9791debfe241cb6baa4ddb58c93ee141a2c02
Alex1ssss <alexissanchez99998@gmail.com>
2024-10-15 12:00:15 -0600

    Add files via upload

--- grupo5i/alumno/models.py
+++ grupo5i/alumno/models.py
@@ -8,3 +8,13 @@ class Alumno(models.Model):
     nombre=models.CharField(max_length=100,verbose_name="Nombree (s)")
     fecha_nacimiento=models.DateField(verbose_name="fecha de nacimiento",null=False,blank=False)
     fecha_ingreso=models.DateField(verbose_name="fecha de ingreso",null=False,blank=False)
+
+    class Meta:
+        db_table="Alumnos"
+        verbose_name="Alumno"
+        verbose_name_plural="Alumnosjaja"
+
+
+class Frase(models.Model):
+    Comentario=models.TextField(verbose_name="comentario")
+    Alumno_fk=models.ForeignKey(Alumno, on_delete=models.CASCADE)
